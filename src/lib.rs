deef44048472d3b06ea8e74c322c3a79ce54ca65
Liqiangcc <43640935+liqiangcc@users.noreply.github.com>
2026-06-19 16:22:53 +0800

    feat: export scanner and executor POC modules

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,19 @@
 
 mod mcp_server;
 mod model;
+mod scan_executor;
+mod scanner;
 
 #[cfg(target_os = "linux")]
 mod safe_fs;
 
 pub use mcp_server::LogQueryServer;
 pub use model::*;
+pub use scan_executor::{ScanExecutor, ScanTaskError};
+pub use scanner::{
+    MAX_LINE_PREVIEW_BYTES, MAX_READ_BUFFER_BYTES, MAX_RETURNED_CONTENT_BYTES, MAX_SCAN_RESULTS,
+    ScanError, ScanLimits, ScanMatch, ScanOutcome, ScanRequest, ScanStopReason, scan_reader,
+};
 
 #[cfg(target_os = "linux")]
 pub use safe_fs::{FileIdentity, SafeFile, SafeOpenError, SafeRoot};
